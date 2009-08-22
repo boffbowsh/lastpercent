4f948aceeb524dd23d3f70623c5b16d09a8b8aed
Andrew Nesbitt <andrewnez@gmail.com>
2009-08-22 04:07:14 +0100

    Reorderd and reduced routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :password_resets
-  map.resources :users
-  map.root      :controller => "users"
-  
-  map.login     '/login',                     :controller => "user_sessions", :action => "new", :conditions => {:method => :get}
-  map.logout    '/logout',                    :controller => "user_sessions", :action => "destroy"
+  map.resources :password_resets, :except => [:index, :show, :destroy]
+  map.resources :users, :except => [:destroy]
+
+  map.login     '/login',   :controller => "user_sessions", :action => "new", :conditions => {:method => :get}
+  map.logout    '/logout',  :controller => "user_sessions", :action => "destroy"
   map.resource  :user_session, :as => 'login'
 
-  map.activate  '/activate/:activation_code', :controller => 'users',         :action => 'activate'
+  map.activate  '/activate/:activation_code', :controller => 'users', :action => 'activate'
+
+  map.root      :controller => "users"
 end
