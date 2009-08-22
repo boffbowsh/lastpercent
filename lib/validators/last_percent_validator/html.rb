64c35b56268c54fed7f73f854386d85f90ae0379
Andrew Nesbitt <andrewnez@gmail.com>
2009-08-22 13:50:51 +0100

    added base of validators

---
+++ lib/validators/last_percent_validator/html.rb
@@ -0,0 +1,9 @@
+class LastPercentValidator::HTML < LastPercentValidator::Base
+    
+  def run
+    # DO ALL YOUR VALIDATION WORK HERE
+    # FOR THE LOVE OF GOD, DO IT HERE
+    # @results << { :severity => SEVERITY[:info], :line_no => line_number, :column_no => column_number, :body => message }
+  end
+
+end
