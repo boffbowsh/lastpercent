e17c1b39cdd896a74af3928e51f20fbc64a1d3a8
Andrew Nesbitt <andrewnez@gmail.com>
2009-08-22 16:44:39 +0100

    Small spider refactoring

--- app/models/content_type.rb
+++ app/models/content_type.rb
@@ -5,8 +5,10 @@ class ContentType < ActiveRecord::Base
   # Validations
   validates_presence_of :mime_type
   validates_uniqueness_of :mime_type
-  
-  def self.sanitize( dirty_mime_type )
-    dirty_mime_type.split(';')[0] 
+
+  before_save :sanitize
+
+  def sanitize
+    self.mime_type = self.mime_type.split(';').first
   end
 end
--- lib/spider.rb
+++ lib/spider.rb
@@ -1,25 +1,21 @@
 class Spider
-  
   def self.crawl( site )
-  
     anemone = Anemone.crawl( site.url ) do |core|
       core.on_every_page do |page|
         asset = site.assets.find_or_create_by_url( page.url.to_s )
-        
-        content_type = ContentType.find_or_create_by_mime_type( ContentType.sanitize(page.content_type) )
-        
-        asset.update_attributes( :body => page.doc.to_s, 
-                                 :responce_status => page.code, 
+
+        content_type = ContentType.find_or_create_by_mime_type( page.content_type )
+
+        asset.update_attributes( :body => page.doc.to_s,
+                                 :responce_status => page.code,
                                  :external => page.external, 
                                  :content_type_id => content_type.id )
-                                 
+
         page.links.each do |link|
           child_asset = site.assets.find_or_create_by_url( link.to_s )
           asset.links << child_asset unless asset.link_ids.include?( child_asset.id )
         end
       end
     end
-    
   end
-  
 end
