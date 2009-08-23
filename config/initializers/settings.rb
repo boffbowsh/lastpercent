976ee0a22732a9ad027b71ab2d4e5d773261eddf
Paul Bowsher <paul.bowsher@gmail.com>
2009-08-23 21:14:09 +0100

    Remove the settings blatter

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,9 +1,9 @@
 begin
-  Settings.url_limit = 70
-  Settings.max_site_limit = 5
-  Settings.css_uri = 'http://127.0.0.1:8180/css-validator/validator'
-  Settings.html_uri = 'http://127.0.0.1/cgi-bin/check'
-  Settings.feed_uri = 'http://127.0.0.1/cgi-bin/check.cgi'
+  #Settings.url_limit = 70
+  #Settings.max_site_limit = 5
+  #Settings.css_uri = 'http://127.0.0.1:8180/css-validator/validator'
+  #Settings.html_uri = 'http://127.0.0.1/cgi-bin/check'
+  #Settings.feed_uri = 'http://127.0.0.1/cgi-bin/check.cgi'
 rescue
   RAILS_DEFAULT_LOGGER.error "You don't have a settings table yet, rake db:migrate!"
 end
