604f142642c85e704e5caf8bed5f12e58b2a934b
Peter Lambert <inneroptics@gmail.com>
2009-08-23 13:35:56 +0100

    Abstracting SEVERITY constant into initializer incase the lib changes, added helper method for getting the severity id from symbol.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,17 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def severity(type)
+    ERROR_SEVERITY[type] rescue 1
+  end
+  
+  def set_asset_class(asset)
+    klass = []
+    klass << 'asset'
+    klass << asset.content_type.to_pretty_s if asset.content_type
+    klass << 'slow' if asset.slow?
+    klass << 'external' if asset.external?
+    klass << cycle('odd', 'even')
+    
+    return klass.join(' ')
+  end
 end
