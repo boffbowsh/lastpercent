2218d653da523adb81da6eede06bdd531b5b54fc
Peter Lambert <inneroptics@gmail.com>
2009-08-23 03:11:48 +0100

    Adding initial js for showing open id field on sign up page.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,15 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+  var open_id_field = $('form#new_user #field_openid_identifier');
+  
+  // Hide the open id field
+  open_id_field.hide();
+  
+  $('form#new_user')
+    .prepend('<a href="#" id="open_id_login_toggle">Sign in with Open ID</a>')
+    .find('#open_id_login_toggle')
+    .click(function() {
+      
+    open_id_field.show();
+    return false;
+  });
+});
