71cfc8d49221668d6bec2e84d4156c04f359d823
Andrew Nesbitt <andrewnez@gmail.com>
2009-08-23 21:55:09 +0100

    OMG!!!

--- app/models/site.rb
+++ app/models/site.rb
@@ -38,7 +38,34 @@ class Site < ActiveRecord::Base
   end
   
   def successes_count
-    0
+    assets_without_errors.count
+  end
+
+  # TODO: REWRITE ME OMG REWRITE ME
+  def assets_with_errors
+    Asset.find(:all, :select => 'distinct assets.*', :joins => 'INNER JOIN results ON results.asset_id = assets.id', :conditions => ['assets.site_id = ? AND results.severity = 2', self.id])
+  end
+
+  # TODO: REWRITE ME OMG REWRITE ME
+  def assets_with_warnings
+    Asset.find(:all, :select => 'distinct assets.*', :joins => 'INNER JOIN results ON results.asset_id = assets.id', :conditions => ['assets.site_id = ? AND results.severity = 1', self.id])
+  end
+
+  # TODO: REWRITE ME OMG REWRITE ME
+  def assets_without_errors
+    Asset.find(:all, :select => 'distinct assets.*', :joins => 'LEFT OUTER JOIN results ON results.asset_id = assets.id', :conditions => [' assets.site_id = ? AND results.id IS NULL', self.id])
+  end
+
+  def percentage_of_assets_with_errors
+    assets_with_errors.size.to_f / assets_count
+  end
+
+  def percentage_of_assets_with_warnings
+    assets_with_warnings.size.to_f / assets_count
+  end
+
+  def percentage_of_assets_without_errors
+    assets_without_errors.size.to_f / assets_count
   end
 
   after_create :enqueue
