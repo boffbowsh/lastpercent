44dc90cb622be394e087aff679479e2c3133bafe
Paul Bowsher <paul.bowsher@gmail.com>
2009-08-23 20:22:03 +0100

    Fix twattishness

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -23,11 +23,6 @@ class Asset < ActiveRecord::Base
 
   after_save :save_local_copy
   
-  def initialize
-    destroy_local_copy!
-    super
-  end
-
   named_scope :has_content_type, :conditions => 'content_type_id IS NOT NULL'
   named_scope :has_no_content_type, :conditions => 'content_type_id IS NULL'
 
