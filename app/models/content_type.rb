a20ce575b0866599464168d973fa03f912ad95b6
Neil Chandler <neil.chandler5@ntlworld.com>
2009-08-23 01:26:17 +0100

    Removed ContentType sanitized methods

--- app/models/content_type.rb
+++ app/models/content_type.rb
@@ -7,14 +7,4 @@ class ContentType < ActiveRecord::Base
   validates_presence_of :mime_type
   validates_uniqueness_of :mime_type
 
-  before_save :sanitize
-
-  def sanitize
-    self.mime_type = ContentType.sanitize self.mime_type
-  end
-
-  def self.sanitize(dirty_mime_type)
-    return "" if dirty_mime_type.nil?
-    dirty_mime_type.split(';').first
-  end
 end
--- lib/spider.rb
+++ lib/spider.rb
@@ -4,7 +4,7 @@ class Spider
       core.on_every_page do |page|
         asset = site.assets.find_or_create_by_url( page.url.to_s )
 
-        content_type = ContentType.find_or_create_by_mime_type( ContentType.sanitize page.content_type )
+        content_type = ContentType.find_or_create_by_mime_type( page.content_type )
 
         asset.update_attributes( :body => page.doc.to_s,
                                  :response_status => page.code,
@@ -13,6 +13,7 @@ class Spider
                                  :content_length => page.content_length,
                                  :response_time => page.response_time )
 
+ 
         page.links.each do |link|
           child_asset = site.assets.find_or_create_by_url( link.to_s )
           asset.links << child_asset unless asset.link_ids.include?( child_asset.id )
