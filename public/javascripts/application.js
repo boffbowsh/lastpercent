48f7871c97886fb75b83c09d2b5a05403f2d8388
Andrew Nesbitt <andrewnez@gmail.com>
2009-08-23 11:28:05 +0100

    whitespace ftw

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   var open_id_field = $('form#new_user #field_openid_identifier');
-  
+
   // Hide the open id field
   open_id_field.hide();
-  
+
   $('form#new_user')
     .prepend('<a href="#" id="open_id_login_toggle">Sign in with Open ID</a>')
     .find('#open_id_login_toggle')
