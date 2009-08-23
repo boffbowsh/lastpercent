2cca07041e40e62edb861172f7c4f1a0060fe2f3
Peter Lambert <inneroptics@gmail.com>
2009-08-23 14:58:48 +0100

    Adding first and last class names to breadcrumbs.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,11 +21,17 @@ module ApplicationHelper
   def breadcrumbs(*args)
     links = [link_to('Home', root_path)]
     links = links + args
+    
+    
     content_for :breadcrumbs do
       content_tag :ul, :id => 'breadcrumbs' do
         returning String.new do |s|
-          links.each do |link|
-            s << content_tag(:li, link)
+          links.each_with_index do |link, i|
+            class_names = []  
+            class_names << 'first' if link == links.first
+            class_names << 'last' if link == links.last
+            
+            s << content_tag(:li, link, :class => class_names.join(' '))
           end
         end
       end
